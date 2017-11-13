febd4f0d3aeafd6221f77d45c93ffdc84fb829f9
Joachim Nilsson <troglobit@gmail.com>
2017-11-13 23:46:46 +0100

    Refactor unit test framework, break out pre() and post() macros
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- src/util.h
+++ src/util.h
@@ -9,56 +9,58 @@
 #include <sys/time.h>
 #include <sys/types.h>
 
-#define dd(fmt, ...) printf(fmt "\n", ##__VA_ARGS__)
+#define dd(fmt, args...)                        \
+    printf(fmt "\n", ##args)
 
-#define die(fmt, ...)                                                         \
-    do {                                                                      \
-        fprintf(stderr, fmt "\n", ##__VA_ARGS__);                             \
-        exit(1);                                                              \
+#define die(fmt, args...)                       \
+    do {                                        \
+        fprintf(stderr, fmt "\n", ##args);      \
+        exit(1);                                \
     } while (0)
 
+#define test_read_pre()                         \
+    unlink("t");                                \
+    system("echo 1 >t");                        \
+                                                \
+    int fd = open("t", O_RDWR, 0640);           \
+    if (fd < 0)                                 \
+        die("open");                            \
+                                                \
+    errno = 0
 
-#define test_pread(statement)                                                 \
-    int main(int argc, char **argv) {                                         \
-                                                                              \
-        unlink("t");                                                          \
-        system("echo 1 >t");                                                  \
-                                                                              \
-        int fd = open("t", O_RDWR, 0640);                                     \
-        if (fd < 0) {                                                         \
-            die("open");                                                      \
-        }                                                                     \
-                                                                              \
-        errno = 0;                                                            \
-        statement                                                             \
-                                                                              \
-        close(fd);                                                            \
-        dd("passed");                                                         \
-        unlink("t");                                                          \
-                                                                              \
-        return 0;                                                             \
+#define test_write_pre()                        \
+    int fd = open("t", O_CREAT | O_EXCL |       \
+        O_SYNC  | O_RDWR | O_TRUNC, 0640);      \
+    if (fd < 0)                                 \
+        die("error open")
+
+#define test_readwrite_post()                   \
+    close(fd);                                  \
+    dd("passed");                               \
+    unlink("t");                                \
+                                                \
+    return 0
+
+#define test_pread(statement)                   \
+    int main(void)                              \
+    {                                           \
+        test_read_pre();                        \
+        statement;                              \
+        test_readwrite_post();                  \
     }
 
-#define test_pwrite(statement)                                                \
-    int main(int argc, char **argv) {                                         \
-                                                                              \
-        int fd = open("t", O_CREAT | O_EXCL | O_SYNC | O_RDWR | O_TRUNC,      \
-                      0640);                                                  \
-        if (fd < 0) {                                                         \
-            die("error open");                                                \
-        }                                                                     \
-                                                                              \
-        statement                                                             \
-                                                                              \
-        close(fd);                                                            \
-        dd("passed");                                                         \
-        unlink("t");                                                          \
-                                                                              \
-        return 0;                                                             \
+#define test_pwrite(statement)                  \
+    int main(void)                              \
+    {                                           \
+        test_write_pre();                       \
+        statement;                              \
+        test_readwrite_post();                  \
     }
 
-static inline int64_t get_now_ms() {
+static inline int64_t get_now_ms(void)
+{
     struct timeval t0;
+
     gettimeofday(&t0, NULL);
     return t0.tv_sec * 1000 + t0.tv_usec / 1000;
 }
